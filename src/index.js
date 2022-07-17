b598774d0879d2e7a7a39eb5df8b5b70ac9f7600
jingyuan.yang <jingyuan.yang@prnasia.com>
2022-07-17 21:09:23 +0800

    feat: jsx初认识

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+let element = <h1 title={'123'}>Hello</h1>;
+
+function greeting(name) { 
+  if (name) {
+    return <h1>HELLO,<span> { name }</span></h1>
+  } else { 
+    return <h1>hello, jingyuan</h1>
+  }
+}
+
+let element2 = greeting('weiwie');
+console.log('element', element);
+console.log('element', element2);
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  element2
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
